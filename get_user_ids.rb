b15e94cfdfea02eb46d63064a3b3884b56231a35
Matthew Heston <matthewheston@gmail.com>
2012-09-22 20:28:46 -0500

    Use batching in requests.
    This takes advantage of the new Twitter gem's better support for
    batching. It also adds a command line option for the script to specify
    the batch size, but be default it will use 100, which is the maximum.
    (The command line option could be removed...I'm not sure why someone
    would want to specify a lower number, but if that use case exists it is
    possible right now.) Some updates to keep track of how many screen names
    do not exist had to be done (basically, we can't just increase the
    not_found variable in the caught exception.)

--- get_user_ids.rb
+++ get_user_ids.rb
@@ -1,6 +1,25 @@
 require 'twitter'
 require File.join(File.expand_path(File.dirname(__FILE__)), ".", "constants.rb") # config info like Twitter username and ID
 
+#for our command line options
+options = {}
+options[:batch] = 100
+
+optparse = OptionParser.new do |opts|
+
+  opts.on('-b', '--batch SIZE', 'Batch size to use in request.') do |size|
+    options[:batch] = size.to_i
+  end
+
+  opts.on( '-h', '--help', 'Display this screen' ) do
+    puts opts
+    exit
+  end
+
+end
+
+optparse.parse!
+
 input_file = ARGV[0]
 output_file = ARGV[1]
 
@@ -24,20 +43,34 @@ client = Twitter::Client.new(:oauth_access =>
 
 counter = 0
 not_found = 0
+found = 0
 puts "starting screen name lookup"
 begin
   screen_names = IO.readlines(input_file)
   of = File.open(output_file,"w+")
-  screen_names.each do |name|
+  screen_names.each_slice(options[:batch]) do |names|
     begin
-      counter+=1
-      user = client.user(name.chomp) # chomp removes any carriage breaks
-      of.puts "\"" + user.screen_name + "\", " + user.id.to_s
-    rescue Twitter::Error::NotFound
-      not_found+=1
+      counter+=names.count
+      name_list = names.map { |name| name.chomp }
+      users = client.users(name_list) # chomp removes any carriage breaks
+      users.each do |user|
+        of.puts "\"" + user.screen_name + "\", " + user.id.to_s
+        found += 1 if user != nil
+      end
+    rescue Twitter::Error::NotFound => e
+      not_found += 1
+      # note: it looks like this would only occur
+      # if we were looking up on screen name and it
+      # fails. if we are looking up a list, it will
+      # just fail silently on those that don't exist.
       of.puts "\"" + name.chomp + "\", \"not found\""
     end
   end
+
+  if counter > 0 and counter != found then
+    not_found = counter - found
+  end
+
 rescue => err
   puts "Exception: #{err}"
 end
